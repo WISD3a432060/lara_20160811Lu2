55b07e81d0bcaa7de78bec1b3350b195e5cc9847
M511-29\admin <a9101021249@gmail.com>
2017-10-11 12:19:21 +0800

    設定 Route 前置

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,14 @@ Route::get('hello/{name?}',['as'=>'hello.index',function($name= 'Everybody'){
 Route::get('say/{name?}',['as'=>'hello.index',function($name='Everybody'){
     return view('welcome');
 }]);
+Route::get('dashboard',function(){
+    return'dashboard';
+}]);
+Route::group(['prefix'=>'admin'],function(){
+    Route::get('dashboard',function(){
+        return'admindashboard';
+    });
+});
 
 
 
