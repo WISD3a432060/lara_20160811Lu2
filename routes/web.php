8165566398661a38bb1e3ff54efda37791eab1c4
M511-29\admin <a9101021249@gmail.com>
2017-10-11 12:18:30 +0800

    為 Route 命名

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,13 @@ Route::get('/',function(){
 Route::get('/',function(){
     return redirect('welcome');
 });
-Route::get('hello/{name}',function($name){
-    return 'Hello,' . $name;
-});
+Route::get('hello/{name?}',['as'=>'hello.index',function($name= 'Everybody'){
+    return'Hello,'.$name;
+}]);
+Route::get('say/{name?}',['as'=>'hello.index',function($name='Everybody'){
+    return view('welcome');
+}]);
+
 
 
 
